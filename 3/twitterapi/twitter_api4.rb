e7cbc880c009daded69514dd207a82040f7c9345
Charlie Williams <c@charliewilliams.org>
2015-03-11 14:31:05 +0000

    Twitter timeline read

--- 3/twitterapi/twitter_api4.rb
+++ 3/twitterapi/twitter_api4.rb
@@ -9,15 +9,23 @@ require 'json'
 baseurl = "https://api.twitter.com"
 path    = "/1.1/statuses/user_timeline.json"
 query   = URI.encode_www_form(
-    "screen_name" => "twitterapi",
+    "screen_name" => "buildsucceeded",
     "count" => 5,
 )
 address = URI("#{baseurl}#{path}?#{query}")
 request = Net::HTTP::Get.new address.request_uri
+twitter_api_key = "F3IDCTfrPYR8aupves2KUU1sj"
+twitter_api_secret = "qaqRVc9eeCfESFSesDkrlEDmpYtOQZDXd7jGbwjdIH1e2OiJO5"
+twitter_access_token = "141585159-dQHdNsPq4fgYPsuH2mJ0xezKN9rCQQHdpGUN3hYx"
+twitter_access_secret = "1FNtCn7AxkcsFPd2KeaETPKpoZQYd3WeINuOpJbqPvdPM"
 
 # Print data about a list of Tweets
 def print_timeline(tweets)
-  # ADD CODE TO ITERATE THROUGH EACH TWEET AND PRINT ITS TEXT
+  
+    puts tweets [0]["user"]["screen_name"]
+    puts tweets [0]["text"]
+
+  
 
 end
 
@@ -26,12 +34,9 @@ http             = Net::HTTP.new address.host, address.port
 http.use_ssl     = true
 http.verify_mode = OpenSSL::SSL::VERIFY_PEER
 
-# If you entered your credentials in the first
-# exercise, no need to enter them again here. The
-# ||= operator will only assign these values if
-# they are not already set.
-consumer_key ||= OAuth::Consumer.new "ENTER IN EXERCISE 1", ""
-access_token ||= OAuth::Token.new "ENTER IN EXERCISE 1", ""
+# Set up OAuth
+consumer_key = OAuth::Consumer.new(twitter_api_key, twitter_api_secret)
+access_token = OAuth::Token.new(twitter_access_token, twitter_access_secret)
 
 # Issue the request.
 request.oauth! http, consumer_key, access_token
@@ -43,5 +48,8 @@ tweets = nil
 if response.code == '200' then
   tweets = JSON.parse(response.body)
   print_timeline(tweets)
+  else
+  puts response.code
+  puts JSON.parse(response.body)
 end
 nil
